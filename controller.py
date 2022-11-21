6482d9453a9c44aefe3e4ef8bdcaecff5f8254ba
Youssef <youssef.donia@gmail.com>
2022-11-20 21:11:59 -0600

    added base code for server

--- controller.py
+++ controller.py
@@ -2,3 +2,6 @@
 # to request files from the server
 # to send a file to renderer to be rendered
 # to be able to control the rendering e.g pause and resume the rendering
+
+import socket
+import sys
--- server.py
+++ server.py
@@ -4,3 +4,57 @@
 # to receive one file at a time from the renderer
 # to be able to stream and store files
 # support text files (and maybe audio files if we have time)
+import socket
+from socket import *
+import sys
+
+serverPort = 1234
+
+# function to launch the server
+
+
+def launch():
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+
+    # we need to control socket behavior, so add setsockopt method
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+    print("Server initialized")
+
+    # assign IP address and port number to this socket instance
+    try:
+        serverSocket.bind(('', serverPort))
+        print("IP address assigned!")
+    except error as errorMessage:
+        print("Failed to assign IP address due to " + str(errorMessage))
+        sys.exit()
+
+    # TODO: should we have a value in the parameter to determine max connections?
+    serverSocket.listen()
+    print("waiting for requests. . .")
+
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+
+        try:
+            message = connectionSocket.recv(1024)
+            print("Recieved message: " + message)
+
+            # CODE FOR FUNCTIONS OF THE SERVER
+
+            # if requested for files to send to controller
+
+            # if requested to send a file to renderer
+
+            # if requested to end
+
+            connectionSocket.close()
+            print("connection closed")
+
+        except IOError as errorMsg:
+            print("Error " + str(errorMsg))
+            # TODO: SEND THE ERROR MESSAGE TO THE CLIENT (CONTROLLER)
+
+
+if __name__ == "__main__":
+    launch()
