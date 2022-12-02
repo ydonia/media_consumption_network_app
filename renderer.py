b2b6c60dcdce52d3b11087b3e8800ce2acab0cf3
alihilal24 <alihilal24@gmail.com>
2022-12-01 18:38:23 -0600

    updated and renamed renderer with entitiyFunction

--- renderer.py
+++ renderer.py
@@ -3,61 +3,57 @@
 # to render that file
 # to send that file to the server to be streamed
 import json
-from entityFunctions import Addresses, MessageTypes, Ports
 import socket
 import sys
 import entityFunctions
 from entityFunctions import *
 
 def rendererStart():
-    print("runs")
-    renderSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    renderSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    rSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print("Created the Render Socket")
 
     try:
-        renderSocket.bind("", Ports.RENDERER)
+        rSocket.bind(('', Renderer.Port))
         print("Bind complete")
-
 
     except socket.error:
         print("Binding Error, exiting program...")
         sys.exit()
 
-    renderSocket.listen(2)
+    rSocket.listen(2)
 
     enabled = True
     while enabled:
-        connection, address = renderSocket.accept()
+        connection, address = rSocket.accept()
         print("incoming address: " + str(address))
 
         try:
             message = connection.recv(1024)
             print(str(message))
             message = entityFunctions.json_loads_byteified(message)
-            messageType = message.get("type")
+            messageType = message.get("function")
 
-            if messageType is MessageTypes.REQUEST:
+            if messageType is Renderer.SEE_FILE_CONTENTS:
                 payload = json.dumps(message)
-                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client.connect((Addresses.SERVER, Ports.SERVER))
-                client.sendall(payload)
-                payload = client.recv(2048)
 
+                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client.connect((Server.Address, Server.Port))
+                client.sendall(payload)
+                
+                payload = client.recv(2048)
                 payload = entityFunctions.json_loads_byteified(payload)
 
-                if payload["type"] is MessageTypes.ERROR:
-                    print(payload["content"])
+                if payload["function"] is ERROR_CODE:
+                    print(payload["data"])
                 else:
-                    print("File: " + payload["fileName"])
-                    print(payload["content"])
+                    print("File: " + payload["file_name"])
+                    print(payload["data"])
             
-            elif messageType is MessageTypes.EXIT:
+            elif messageType is EXIT_CODE:
                 enabled = False
-                data = json.dumps({"content": "[RENDERER] shutting down"})
+                data = json.dumps({"data": "[RENDERER] shutting down"})
                 connection.sendall(data)
-
-
 
             print("Connection closing...")
             client.close()    
@@ -66,8 +62,8 @@ def rendererStart():
         except IOError as e:
             print(str(e))
             connection.close()
-    renderSocket.shutdown()
-    renderSocket.close()
+    rSocket.shutdown()
+    rSocket.close()
     print("Program Closing...")
 
 
